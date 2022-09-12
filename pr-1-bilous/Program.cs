238f1e3aaf641f5093a13d158009b5557d9ca1bf
MARKUS <89963327+twenntty@users.noreply.github.com>
2022-09-12 16:55:49 +0300

    First lab

---
+++ pr-1-bilous/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program
+{
+    class Program
+    {
+        public static void Main()
+        {
+            float num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Число парне!");
+            }
+            else
+            {
+                Console.WriteLine("Число непарне");
+            }
+        }
+    }
+    
+}
