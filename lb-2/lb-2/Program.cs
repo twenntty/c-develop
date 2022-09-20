4a444f09fcf9bdac010e18e36250de276a2f3d32
MARKUS <89963327+twenntty@users.noreply.github.com>
2022-09-20 18:20:04 +0300

    lb-2

---
+++ lb-2/lb-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lb
+{
+    class lb
+    {
+        public static void Main()
+        {
+            int nn = 0;
+            int nk = 0;
+            int k = 0;
+            double result = 0;
+
+            bool isNNok = false;
+            bool isNKok = false;
+
+            do
+            {
+                Console.Write("Введіть значення NN. NN = ");
+                string strNN = Console.ReadLine();
+                isNNok = int.TryParse(strNN, out nn);
+                if (!isNNok && nn <= 0)
+                {
+                    Console.WriteLine("Помилка введення.");
+                }
+            } while (!isNNok);
+
+            Console.Write("Введіть значення NK. NK = ");
+            string strNK = Console.ReadLine();
+            isNKok = int.TryParse(strNK, out nk);
+            if (!isNKok && nk < nn)
+            {
+                Console.WriteLine("Помилка введення");
+                Console.ReadLine();
+                return;
+            }
+
+            for (k = nn; k< nk; k++)
+            {
+                result += Math.Pow(k, 2) + Math.Pow(-1, k) * k - 1 / Math.Pow(k, 2) + 1;
+            }
+            Console.Write("Результат = {0}", result);
+        }
+    }
+}
