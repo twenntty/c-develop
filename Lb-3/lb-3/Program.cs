737d3840e0ea9736d6d90b3fd69313c83d99b73d
MARKUS <89963327+twenntty@users.noreply.github.com>
2022-09-24 10:31:11 +0300

    lb-3

---
+++ Lb-3/lb-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace program
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть число х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть число у: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if(y > 0 && x < 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
